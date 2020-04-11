72a1d367452370aad765ed39d3e5e7c36ddeddd9
Jose Wolff <josewolff12@gmail.com>
2020-04-11 09:49:05 -0600

    add new route

--- TestVS/Controllers/Users.cs
+++ TestVS/Controllers/Users.cs
@@ -8,7 +8,7 @@ using TestVS.Models;
 namespace TestVS.Controllers
 {
 
- 
+
 
     [Route("api/[controller]")]
     [ApiController]
@@ -26,14 +26,15 @@ namespace TestVS.Controllers
             }
         }
 
-        // GET api/users/5
+        // GET api/users/<username>
+        [Route("findByUserName/{userName}")]
         [HttpGet("{userName}")]
         public Object Get(String userName)
         {
             using (DbConnection db = new DbConnection())
             {
                
-                Users result = db.Users.FirstOrDefault(User => User.UserName.ToUpper().Equals(userName.ToUpper()));
+                Users result = db.Users.FirstOrDefault(User => User.UserName.ToLower().Equals(userName.ToLower()));
                 if(result == null)
                 {
                     String notFoundMessage = "{\"status\":0,\"Message\":\"The UserName " + userName + " not exists.\"}";
@@ -42,6 +43,43 @@ namespace TestVS.Controllers
                 return result;
             }
         }
+
+        // GET api/users/5
+        [Route("findByUserId/{id}")]
+        [HttpGet("{id}")]
+        public Object GetById(int id)
+        {
+            using (DbConnection db = new DbConnection())
+            {
+
+                Users result = db.Users.FirstOrDefault(User => User.Id == id);
+                if (result == null)
+                {
+                    String notFoundMessage = "{\"status\":0,\"Message\":\"The UserId " + id + " not exists.\"}";
+                    return Content(notFoundMessage, "application/json");
+                }
+                return result;
+            }
+        }
+
+        // GET api/users/5
+        [Route("usernameContains('{content}')")]
+        [HttpGet("{content}")]
+        public Object GetByMatched(String content)
+        {
+            using (DbConnection db = new DbConnection())
+            {
+
+                List<Users> result = db.Users.Where(User => User.UserName.ToLower().Contains(content.ToLower())).ToList();
+                if (result.Count == 0)
+                {
+                    String notFoundMessage = "{}";
+                    return Content(notFoundMessage, "application/json");
+                }
+                return result;
+            }
+        }
+
 
         // POST api/users
         [Route("add")]
@@ -70,24 +108,49 @@ namespace TestVS.Controllers
         }
 
 
-        // DELETE api/users/5
-        [Route("remove/{userName}")]
+        // DELETE api/users/<username>
+        [Route("removeByUserName/{userName}")]
         [HttpDelete("{userName}")]
-        public Object Delete(String userName)
+        public Object DeleteByUserName(String userName)
         {
             String deleteResult = "";
             using (DbConnection db = new DbConnection())
             {
 
-                if (db.Users.Where(t => t.UserName.ToUpper().Equals(userName.ToUpper())).Count() > 0)
+                if (db.Users.Where(t => t.UserName.ToLower().Equals(userName.ToLower())).Count() > 0)
                 {
                     deleteResult = "{\"status\":1,\"Message\":\"UserName: " + userName + " deleted.\"}";
-                    db.Users.Remove(db.Users.First(t => t.UserName.ToUpper().Equals(userName.ToUpper())));
+                    db.Users.Remove(db.Users.First(t => t.UserName.ToLower().Equals(userName.ToLower())));
                     db.SaveChanges();
                 }
                 else
                 {
                     deleteResult = "{\"status\":0,\"Message\":\"The UserName: " + userName + " doesnt exists.\"}";
+                }
+
+            }
+            return Content(deleteResult, "application/json");
+        }
+
+
+        // DELETE api/users/<userid>
+        [Route("removeByUserId/{id}")]
+        [HttpDelete("{id}")]
+        public Object DeleteByUserId(int id)
+        {           
+            String deleteResult = "";
+            using (DbConnection db = new DbConnection())
+            {
+
+                if (db.Users.Where(t => t.Id == id).Count() > 0)
+                {
+                    deleteResult = "{\"status\":1,\"Message\":\"UserId: " + id + " deleted.\"}";
+                    db.Users.Remove(db.Users.First(t => t.Id == id));
+                    db.SaveChanges();
+                }
+                else
+                {
+                    deleteResult = "{\"status\":0,\"Message\":\"The UserId: " + id + " doesnt exists.\"}";
                 }
 
             }
--- TestVS/Program.cs
+++ TestVS/Program.cs
@@ -19,6 +19,7 @@ namespace TestVS
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5000;https://localhost:5001;");
     }
 }
